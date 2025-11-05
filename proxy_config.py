a3f673ac24bc86cd365c18e246ae86f5b8db05b3
moswj0 <mosv.chara@gmail.com>
2025-11-05 12:01:19 +0700

    update code

---
+++ proxy_config.py
@@ -0,0 +1,7 @@
+# Proxy Configuration
+# This file is ignored by git
+
+PROXY_CONFIG = {
+    'http': 'http://npsvsyws-residential-rotate:xq4bjj9hy0wl@p.webshare.io:80',
+    'https': 'http://npsvsyws-residential-rotate:xq4bjj9hy0wl@p.webshare.io:80'
+}
